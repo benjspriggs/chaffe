c7b87ac630ca1b1d4285e1360af10c0bbff2669f
Gully <gully@dain.dreamhost.com>
2016-10-01 23:17:50 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chaffe::Application.config.secret_token = '89ab86b8f35c6df9668d3647cdb38ab6874926aa2c80d040c2a2f760a33d801793823016ee3f942a654e331eff2c73caf941a10a016f283cb0091dd2a453424d'
